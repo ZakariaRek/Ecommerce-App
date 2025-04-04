232098a847adb02b7af1eef62ce7b23526d1d3a7
ZakariaRekhla2004 <zakariaest49@gmail.com>
2025-04-04 00:35:18 +0000

    First

---
+++ Loyalty-Service/src/test/java/com/Ecommerce/Loyalty_Service/LoyaltyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Ecommerce.Loyalty_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoyaltyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
