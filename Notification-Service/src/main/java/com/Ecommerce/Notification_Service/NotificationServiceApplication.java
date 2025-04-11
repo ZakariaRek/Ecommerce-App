367715dad8c5edf69002d196c2b432b8c35c3699
Saoud yahya <Saoudyahya123@gmail.com>
2025-04-11 22:47:25 +0100

    continue on the setup

--- Notification-Service/src/main/java/com/Ecommerce/Notification_Service/NotificationServiceApplication.java
+++ Notification-Service/src/main/java/com/Ecommerce/Notification_Service/NotificationServiceApplication.java
@@ -2,8 +2,10 @@ package com.Ecommerce.Notification_Service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories(basePackages = "com.Ecommerce.Notification_Service.Repositories")
 public class NotificationServiceApplication {
 
 	public static void main(String[] args) {
