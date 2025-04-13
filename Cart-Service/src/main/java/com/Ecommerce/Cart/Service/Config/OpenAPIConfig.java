f50ccd51a883519adbd283b5df9b1db4ef161bf1
Saoud yahya <Saoudyahya123@gmail.com>
2025-04-13 23:08:09 +0100

    Swagger-order proble  in cart -setup

--- Cart-Service/src/main/java/com/Ecommerce/Cart/Service/Config/OpenAPIConfig.java
+++ Cart-Service/src/main/java/com/Ecommerce/Cart/Service/Config/OpenAPIConfig.java
@@ -1,23 +1,37 @@
 package com.Ecommerce.Cart.Service.Config;
 
+
 import io.swagger.v3.oas.models.OpenAPI;
+
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.Contact;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 public class OpenAPIConfig {
 
     @Bean
-    public OpenAPI cartServiceOpenAPI() {
+    public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
         return new OpenAPI()
                 .info(new Info()
-                        .title("Cart Service API")
-                        .description("Shopping cart management API documentation")
-                        .version("1.0.0")
-                        .contact(new Contact()
-                                .name("Your Name")
-                                .email("your.email@example.com")));
+                        .title("APP ")
+                        .version("1.0")
+                        .description("API documentation for RAIC CLU APP"))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                );
     }
 }
