9b1809aeef51d4dc9a67beb6954e63c008029fdc
Saoud yahya <Saoudyahya123@gmail.com>
2025-04-15 20:31:36 +0100

    Swagger-order problem  in cart -setup

--- Cart-Service/src/main/java/com/Ecommerce/Cart/Service/Config/OpenAPIConfig.java
+++ Cart-Service/src/main/java/com/Ecommerce/Cart/Service/Config/OpenAPIConfig.java
@@ -1,13 +1,9 @@
 package com.Ecommerce.Cart.Service.Config;
 
-
 import io.swagger.v3.oas.models.OpenAPI;
-
 import io.swagger.v3.oas.models.info.Info;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
@@ -19,10 +15,11 @@ public class OpenAPIConfig {
     public OpenAPI customOpenAPI() {
         final String securitySchemeName = "bearerAuth";
         return new OpenAPI()
+                .openapi("3.1.0") // Add explicit OpenAPI version
                 .info(new Info()
                         .title("APP ")
                         .version("1.0")
-                        .description("API documentation for RAIC CLU APP"))
+                        .description("API documentation for Cart service  APP"))
                 .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
                 .components(new Components()
                         .addSecuritySchemes(securitySchemeName,
