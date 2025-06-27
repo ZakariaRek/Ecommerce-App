a825fc7f0e1cd685e4158f0e74a507b19035eba3
Saoud yahya <Saoudyahya123@gmail.com>
2025-06-27 17:04:13 +0100

    conf setup all spring services

--- Order-Service/src/main/java/com/Ecommerce/Order_Service/OrderServiceApplication.java
+++ Order-Service/src/main/java/com/Ecommerce/Order_Service/OrderServiceApplication.java
@@ -2,7 +2,9 @@ package com.Ecommerce.Order_Service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties
 @SpringBootApplication
 public class OrderServiceApplication {
 
